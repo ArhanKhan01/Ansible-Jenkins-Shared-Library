1a7c355195f06c5548b9b2daeae5e41eedf61f79
arhan <arhankhan52022@gmail.com>
2026-09-20 00:22:39 +0530

    Add Ansible deployment shared library

---
+++ vars/ansibleDeploy.groovy
@@ -0,0 +1,43 @@
+def call(Map config = [:]) {
+
+    stage('Clone') {
+        echo "Cloning Ansible Assignment-5 repository..."
+
+        git(
+            branch: 'main',
+            url: 'https://github.com/ArhanKhan01/Ansible-Assignment-5.git'
+        )
+    }
+
+    stage('User Approval') {
+
+        if (config.keepApprovalStage) {
+            input(
+                message: "Deploy SonarQube to ${config.environment}?",
+                ok: "Approve Deployment"
+            )
+        }
+    }
+
+    stage('Playbook Execution') {
+
+        echo "Running SonarQube Ansible Playbook..."
+
+        sh """
+            ansible-playbook \
+            ${config.codeBasePath}/site.yml \
+            -i ${config.codeBasePath}/inventory
+        """
+    }
+
+    stage('Notification') {
+
+        echo "Sending deployment notification..."
+
+        slackSend(
+            channel: config.slackChannelName,
+            message: config.actionMessage
+        )
+    }
+}
+
